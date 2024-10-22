3ac75af4010b15359b4876768246ac54c7691be0
Jefferson Miura <105235287+JeffMiura@users.noreply.github.com>
2024-10-22 18:18:30 -0300

    second commit

--- hello.py
+++ hello.py
@@ -1,6 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
 @app.route("/")
 def hello_world():
     return "<p>Hello,World!</p>"
+
+
+@app.route("/products")
+def products():
+    response = jsonify([
+
+        {
+           "title": "Caneca personalizada de Porcelana do backend",
+            "amount": 456.45,
+            "Installments":{"Number":3,"total":41.15}
+        },
+        {
+            "title": "Caneca de Tulipa",
+            "amount": 789.45,
+            "Installments":{"Number":6,"total":41.15,"hasFee": True}
+        }
+    ])
+    response.headers.add('Access-Control-Allow-Origin','*')
+
+    return response
